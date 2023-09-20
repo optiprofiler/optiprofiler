63d4b071d13c8d0be02f3112eb7ebedacd34bc5d
Tom M. Ragonneau <t.ragonneau@gmail.com>
2023-09-20 15:06:47 +0800

    Update PYCUTEST_CACHE

--- .github/actions/test-python/run-tests.sh
+++ .github/actions/test-python/run-tests.sh
@@ -5,7 +5,7 @@ set -x
 
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  export PYCUTEST_CACHE=$GITHUB_WORKSPACE/pycutest_cache
+  export PYCUTEST_CACHE=$GITHUB_WORKSPACE
   python -m pip install --progress-bar=off .[extra,tests]
   python -m pytest --cov=. --cov-report=xml --run-extra
 else
