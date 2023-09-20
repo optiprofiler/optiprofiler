f0e51e65ce2c79767c5953a6f0e1ac5d571938f6
Tom M. Ragonneau <t.ragonneau@gmail.com>
2023-09-20 14:38:12 +0800

    Add missing dependencies

--- .github/actions/test-python/run-tests.sh
+++ .github/actions/test-python/run-tests.sh
@@ -3,9 +3,11 @@
 set -e
 set -x
 
-python -m pip install --progress-bar=off .[tests]
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
+  python -m pip install --progress-bar=off .[extra,tests]
   python -m pytest --cov=. --cov-report=xml --run-extra
 else
+  python -m pip install --progress-bar=off .[tests]
   python -m pytest --cov=. --cov-report=xml
 fi
