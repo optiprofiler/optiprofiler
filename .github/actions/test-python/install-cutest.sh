e7ac98f3f29503aaa230f7c8833fddec50f8fb45
Tom M. Ragonneau <t.ragonneau@gmail.com>
2023-09-21 21:36:41 +0800

    Install CUTEst on macOS

--- .github/actions/test-python/install-cutest.sh
+++ .github/actions/test-python/install-cutest.sh
@@ -3,26 +3,40 @@
 set -e
 set -x
 
-# Download CUTEst and its dependencies
-mkdir "$GITHUB_WORKSPACE/cutest"
-git clone --depth 1 --branch v2.1.24 https://github.com/ralna/ARCHDefs.git "$GITHUB_WORKSPACE/cutest/archdefs"
-git clone --depth 1 --branch v2.0.6 https://github.com/ralna/SIFDecode.git "$GITHUB_WORKSPACE/cutest/sifdecode"
-git clone --depth 1 --branch v2.0.17 https://github.com/ralna/CUTEst.git "$GITHUB_WORKSPACE/cutest/cutest"
-git clone --depth 1 --branch v0.5 https://bitbucket.org/optrove/sif.git "$GITHUB_WORKSPACE/cutest/mastsif"
+if [[ "$RUNNER_OS" == "Linux" ]]; then
+  # Download CUTEst and its dependencies
+  mkdir "$GITHUB_WORKSPACE/cutest"
+  git clone --depth 1 --branch v2.1.24 https://github.com/ralna/ARCHDefs.git "$GITHUB_WORKSPACE/cutest/archdefs"
+  git clone --depth 1 --branch v2.0.6 https://github.com/ralna/SIFDecode.git "$GITHUB_WORKSPACE/cutest/sifdecode"
+  git clone --depth 1 --branch v2.0.17 https://github.com/ralna/CUTEst.git "$GITHUB_WORKSPACE/cutest/cutest"
+  git clone --depth 1 --branch v0.5 https://bitbucket.org/optrove/sif.git "$GITHUB_WORKSPACE/cutest/mastsif"
 
-# Set the environment variables
-export ARCHDEFS="$GITHUB_WORKSPACE/cutest/archdefs"
-export SIFDECODE="$GITHUB_WORKSPACE/cutest/sifdecode"
-export CUTEST="$GITHUB_WORKSPACE/cutest/cutest"
-export MASTSIF="$GITHUB_WORKSPACE/cutest/mastsif"
-export MYARCH=pc64.lnx.gfo
-{
-  echo "ARCHDEFS=$ARCHDEFS"
-  echo "SIFDECODE=$SIFDECODE"
-  echo "CUTEST=$CUTEST"
-  echo "MASTSIF=$MASTSIF"
-  echo "MYARCH=$MYARCH"
-} >> "$GITHUB_ENV"
+  # Set the environment variables
+  export ARCHDEFS="$GITHUB_WORKSPACE/cutest/archdefs"
+  export SIFDECODE="$GITHUB_WORKSPACE/cutest/sifdecode"
+  export CUTEST="$GITHUB_WORKSPACE/cutest/cutest"
+  export MASTSIF="$GITHUB_WORKSPACE/cutest/mastsif"
+  export MYARCH=pc64.lnx.gfo
+  {
+    echo "ARCHDEFS=$ARCHDEFS"
+    echo "SIFDECODE=$SIFDECODE"
+    echo "CUTEST=$CUTEST"
+    echo "MASTSIF=$MASTSIF"
+    echo "MYARCH=$MYARCH"
+  } >> "$GITHUB_ENV"
 
-# Build and install CUTEst
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/jfowkes/pycutest/master/.install_cutest.sh)"
+  # Build and install CUTEst
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/jfowkes/pycutest/master/.install_cutest.sh)"
+elif [[ "$RUNNER_OS" == "macOS" ]]; then
+  # Install gfortran and gcc
+  sudo ln -fs /usr/local/bin/gfortran-12 /usr/local/bin/gfortran
+  sudo ln -fs /usr/local/bin/gcc-12 /usr/local/bin/gcc
+
+  # Install CUTEst
+  brew tap optimizers/cutest
+  brew install cutest --without-single
+  brew install mastsif
+  for f in "archdefs" "sifdecode" "cutest" "mastsif"; do
+    cat "$(brew --prefix $f)/$f.bashrc"
+  done
+fi
--- .github/actions/test-python/run-tests.sh
+++ .github/actions/test-python/run-tests.sh
@@ -3,8 +3,8 @@
 set -e
 set -x
 
-
-if [[ "$RUNNER_OS" == "Linux" ]]; then
+# Run the extra tests on Linux and macOS
+if [[ "$RUNNER_OS" == "Linux" || "$RUNNER_OS" == "macOS" ]]; then
   export PYCUTEST_CACHE=$GITHUB_WORKSPACE
   python -m pip install --progress-bar=off .[extra,tests]
   python -m pytest --cov=. --cov-report=xml --run-extra
