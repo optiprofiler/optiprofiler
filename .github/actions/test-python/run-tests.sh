65e4e4b5ebbdb6263ea8a01de0c1393e15c50e31
Tom M. Ragonneau <t.ragonneau@gmail.com>
2023-12-08 15:01:36 +0800

    Update spelling.yml

--- .github/actions/test-python/run-tests.sh
+++ .github/actions/test-python/run-tests.sh
@@ -6,9 +6,9 @@ set -x
 # Run the extra tests on Linux and macOS
 if [[ "$RUNNER_OS" == "Linux" || "$RUNNER_OS" == "macOS" ]]; then
   export PYCUTEST_CACHE=$GITHUB_WORKSPACE
-  python -m pip install --progress-bar=off .[extra,tests]
+  python -m pip install --progress-bar=off ".[extra,tests]"
   python -m pytest --cov=. --cov-report=xml --run-extra
 else
-  python -m pip install --progress-bar=off .[tests]
+  python -m pip install --progress-bar=off ".[tests]"
   python -m pytest --cov=. --cov-report=xml
 fi
