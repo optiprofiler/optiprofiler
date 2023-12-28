9a179ceaa2c8c0a7bd071f80cd80acf225662340
Huang_Mac <huangcunxin@outlook.com>
2023-12-29 02:15:49 +0800

    Modify workflow and test Matcutest

--- matlab/OptiProfiler/tests/TestSolveAll.m
+++ matlab/OptiProfiler/tests/TestSolveAll.m
@@ -23,28 +23,6 @@ classdef TestSolveAll < matlab.unittest.TestCase
             testCase.verifyGreaterThan(problem_dimensions, 0);
         end
 
-        function testParallelComputing(testCase)
-            problem_names = {'AKIVA', 'BEALE'};
-            problem_options = struct('maxdim', 2);
-            solvers = {@fminsearch_test, @fminunc_test};
-            labels = {'fminsearch', 'fminunc'};
-            feature = Feature('plain');
-            max_eval_factor = 500;
-            profile_options = struct('n_jobs', 2);  % Parallel computing option set to 2 jobs
-
-            % Call the function under test
-            [fun_values, maxcv_values, fun_inits, maxcv_inits, n_evals, problem_names_output, problem_dimensions] = solveAll(problem_names, problem_options, solvers, labels, feature, max_eval_factor, profile_options);
-
-            % Verify the function outputs
-            testCase.verifyNotEmpty(fun_values);
-            testCase.verifyNotEmpty(maxcv_values);
-            testCase.verifyNotEmpty(fun_inits);
-            testCase.verifyNotEmpty(maxcv_inits);
-            testCase.verifyNotEmpty(n_evals);
-            testCase.verifyEqual(problem_names, problem_names_output);
-            testCase.verifyGreaterThan(problem_dimensions, 0);
-        end
-
         function testFevalFewerThanMaxEval(testCase)
             problem_names = {'AKIVA', 'BEALE'};
             problem_options = struct('maxdim', 2);
@@ -67,5 +45,28 @@ classdef TestSolveAll < matlab.unittest.TestCase
             testCase.verifyEqual(size(fun_values), [2 1 1 1000]);
             testCase.verifyEqual(size(maxcv_values), [2 1 1 1000]);
         end
+
+        function testParallelComputing(testCase)
+            problem_names = {'AKIVA', 'BEALE'};
+            problem_options = struct('maxdim', 2);
+            solvers = {@fminsearch_test, @fminunc_test};
+            labels = {'fminsearch', 'fminunc'};
+            feature = Feature('plain');
+            max_eval_factor = 500;
+            profile_options = struct('n_jobs', 2);  % Parallel computing option set to 2 jobs
+
+            % Call the function under test
+            [fun_values, maxcv_values, fun_inits, maxcv_inits, n_evals, problem_names_output, problem_dimensions] = solveAll(problem_names, problem_options, solvers, labels, feature, max_eval_factor, profile_options);
+
+            % Verify the function outputs
+            testCase.verifyNotEmpty(fun_values);
+            testCase.verifyNotEmpty(maxcv_values);
+            testCase.verifyNotEmpty(fun_inits);
+            testCase.verifyNotEmpty(maxcv_inits);
+            testCase.verifyNotEmpty(n_evals);
+            testCase.verifyEqual(problem_names, problem_names_output);
+            testCase.verifyGreaterThan(problem_dimensions, 0);
+        end
+
     end
 end
