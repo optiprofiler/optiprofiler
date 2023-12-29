4a8dfd06a402d9f0f77b35cb351781b40aa72b11
OptHuang <huangcunxin@outlook.com>
2023-12-29 10:48:16 +0800

    Modify test file for enumeration class

--- matlab/OptiProfiler/tests/TestFeatureName.m
+++ matlab/OptiProfiler/tests/TestFeatureName.m
@@ -4,11 +4,22 @@ classdef TestFeatureName < matlab.unittest.TestCase
         function testEnumerationValues(testCase)
             % Test if the enumeration values are correctly assigned
             enumValues = enumeration('FeatureName');
-            expectedValues = {'custom', 'noisy', 'plain', 'regularized', 'tough', 'truncated'};
+            expectedValues = {'CUSTOM', 'NOISY', 'PLAIN', 'REGULARIZED', 'TOUGH', 'TRUNCATED'};
 
-            for i = 1:numel(enumValues)
-                testCase.verifyEqual(enumValues(i).value, expectedValues{i});
-            end
+            diff1 = setdiff(enumValues, expectedValues);
+            diff2 = setdiff(expectedValues, enumValues);
+            testCase.verifyEmpty(diff1, "The enumeration values are not correctly assigned");
+            testCase.verifyEmpty(diff2, "The enumeration values are not correctly assigned");
+        end
+
+        function testConstructor(testCase)
+            % Test if the constructor works as expected
+            testCase.verifyEqual(FeatureName('CUSTOM').value, 'custom');
+            testCase.verifyEqual(FeatureName('NOISY').value, 'noisy');
+            testCase.verifyEqual(FeatureName('PLAIN').value, 'plain');
+            testCase.verifyEqual(FeatureName('REGULARIZED').value, 'regularized');
+            testCase.verifyEqual(FeatureName('TOUGH').value, 'tough');
+            testCase.verifyEqual(FeatureName('TRUNCATED').value, 'truncated');
         end
         
     end
