7003fb24b77bede2ae2ae76fa418f0193f10b063
OptHuang <huangcunxin@outlook.com>
2024-01-02 13:24:31 +0800

    Modify tests

--- matlab/OptiProfiler/tests/TestSolveOne.m
+++ matlab/OptiProfiler/tests/TestSolveOne.m
@@ -61,3 +61,11 @@ classdef TestSolveOne < matlab.unittest.TestCase
 
     end
 end
+
+function [x, fval] = fminsearch_test(fun, x0, xl, xu, aub, bub, aeq, beq, cub, ceq, max_eval)
+
+    n = length(x0);
+    options = optimset('MaxFunEvals', max_eval);
+    [x, fval] = fminsearch(fun, x0, options);
+
+end
