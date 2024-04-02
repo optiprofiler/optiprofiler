71a822933874037332e2d4b341c30261f4306000
OptHuang <huangcunxin@outlook.com>
2024-04-02 13:24:52 +0800

    Reload missing files

---
+++ python/optiprofiler/__init__.py
@@ -0,0 +1,28 @@
+from .problems import Problem, get_cutest_problem_options, set_cutest_problem_options, find_cutest_problems
+from .profiles import run_benchmark
+from .utils import show_versions
+
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Final release markers:
+#   X.Y.0   # For first release after an increment in Y
+#   X.Y.Z   # For bugfix releases
+#
+# Admissible pre-release markers:
+#   X.YaN   # Alpha release
+#   X.YbN   # Beta release
+#   X.YrcN  # Release Candidate
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'.
+__version__ = '1.0.dev0'
+
+__all__ = [
+    'Problem',
+    'run_benchmark',
+    'get_cutest_problem_options',
+    'set_cutest_problem_options',
+    'find_cutest_problems',
+    'show_versions',
+]
