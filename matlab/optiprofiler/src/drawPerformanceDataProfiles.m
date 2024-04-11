faaed0eee9c1b13dc2ed09f1b9fa9d5ab43f7476
OptHuang <huangcunxin@outlook.com>
2024-04-11 22:24:55 +0800

    Improve the color cycle for plotting

--- matlab/optiprofiler/src/drawPerformanceDataProfiles.m
+++ matlab/optiprofiler/src/drawPerformanceDataProfiles.m
@@ -1,7 +1,9 @@
 function drawPerformanceDataProfiles(ax, x, y, labels)
 %DRAWPERFORMANCEDATAPROFILES draws performance profiles and data profiles.
 
-    ax.LineStyleCyclingMethod = 'withcolor';
+    set(ax, 'DefaultAxesColorOrder', [0, 0.4470, 0.7410; 0.8500, 0.3250, 0.0980; 0.9290, 0.6940, 0.1250; 0.4940, 0.1840, 0.5560; 0.4660, 0.6740, 0.1880; 0.3010, 0.7450, 0.9330; 0.6350, 0.0780, 0.1840]);
+    set(ax, 'DefaultAxesLineStyleOrder', {'-', '--', ':', '-.'});
+
     n_solvers = size(x, 2);
     n_runs = size(y, 3);
     y_mean = squeeze(mean(y, 3));
--- matlab/optiprofiler/src/runBenchmark.m
+++ matlab/optiprofiler/src/runBenchmark.m
@@ -153,8 +153,6 @@ function runBenchmark(solvers, labels, problem_names, feature_names, varargin)
     set(groot, 'DefaultLineLineWidth', 1);
     set(groot, 'DefaultAxesFontSize', 12);
     set(groot, 'DefaultAxesFontName', 'Arial');
-    set(groot, 'DefaultAxesColorOrder', [0, 0.4470, 0.7410; 0.8500, 0.3250, 0.0980; 0.9290, 0.6940, 0.1250; 0.4940, 0.1840, 0.5560; 0.4660, 0.6740, 0.1880; 0.3010, 0.7450, 0.9330; 0.6350, 0.0780, 0.1840]);
-    set(groot, 'DefaultAxesLineStyleOrder', {'-', '--', ':', '-.'});
 
     if strcmp(feature_names, 'all')
         feature_names = enumeration('FeatureName');
