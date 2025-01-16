d99e79d3ca6e0ae6bd727d7a5561934b525a594a
Huang_Mac <huangcunxin@outlook.com>
2025-01-17 02:57:30 +0800

    test

---
+++ matlab/optiprofiler/tests/action_tests/test_custom.m
@@ -0,0 +1,120 @@
+function test_custom(benchmark_id)
+    % Test "custom" feature that combines "noisy" and "linearly_transformed".
+
+    % Go to the directory of this repository.
+    cd(fullfile(fileparts(mfilename('fullpath')), '..', '..', '..', '..'));
+
+    solvers = {@fmincon_test1, @fmincon_test2};
+    options.solver_names = {'sqp', 'interior-point'};
+    options.feature_name = 'custom';
+    options.n_runs = 3;
+    options.mod_x0 = @mod_x0;
+    options.mod_fun = @mod_fun;
+    options.mod_affine = @mod_affine;
+    options.feature_stamp = 'noisy_linearly_transformed';
+    options.p_type = 'ubln';
+    options.mindim = 11;
+    options.maxdim = 11;
+    options.benchmark_id = benchmark_id;
+
+    benchmark(solvers, options);
+end
+
+function x0 = mod_x0(rand_stream, problem)
+
+    [Q, R] = qr(rand_stream.randn(problem.n));
+    Q(:, diag(R) < 0) = -Q(:, diag(R) < 0);
+    x0 = Q * problem.x0;
+end
+
+function f = mod_fun(x, rand_stream, problem)
+
+    f = problem.fun(x);
+    f = f + max(1, abs(f)) * 1e-3 * rand_stream.randn(1);
+end
+
+function [A, b, inv] = mod_affine(rand_stream, problem)
+
+    [Q, R] = qr(rand_stream.randn(problem.n));
+    Q(:, diag(R) < 0) = -Q(:, diag(R) < 0);
+    A = Q';
+    b = zeros(problem.n, 1);
+    inv = Q;
+end
+
+function x = fmincon_test1(varargin)
+    options = optimoptions('fmincon', 'SpecifyObjectiveGradient', false, 'Algorithm', 'sqp');
+    if nargin == 2
+        fun = varargin{1};
+        x0 = varargin{2};
+        x = fmincon(fun, x0, [], [], [], [], [], [], [], options);
+    elseif nargin == 4
+        fun = varargin{1};
+        x0 = varargin{2};
+        xl = varargin{3};
+        xu = varargin{4};
+        x = fmincon(fun, x0, [], [], [], [], xl, xu, [], options);
+    elseif nargin == 8
+        fun = varargin{1};
+        x0 = varargin{2};
+        xl = varargin{3};
+        xu = varargin{4};
+        aub = varargin{5};
+        bub = varargin{6};
+        aeq = varargin{7};
+        beq = varargin{8};
+        x = fmincon(fun, x0, aub, bub, aeq, beq, xl, xu, [], options);
+    elseif nargin == 10
+        fun = varargin{1};
+        x0 = varargin{2};
+        xl = varargin{3};
+        xu = varargin{4};
+        aub = varargin{5};
+        bub = varargin{6};
+        aeq = varargin{7};
+        beq = varargin{8};
+        cub = varargin{9};
+        ceq = varargin{10};
+        nonlcon = @(x) deal(cub(x), ceq(x));
+        x = fmincon(fun, x0, aub, bub, aeq, beq, xl, xu, nonlcon, options);
+    end
+end
+
+function x = fmincon_test2(varargin)
+
+    options = optimoptions('fmincon', 'SpecifyObjectiveGradient', false, 'Algorithm', 'interior-point');
+    if nargin == 2
+        fun = varargin{1};
+        x0 = varargin{2};
+        x = fmincon(fun, x0, [], [], [], [], [], [], [], options);
+    elseif nargin == 4
+        fun = varargin{1};
+        x0 = varargin{2};
+        xl = varargin{3};
+        xu = varargin{4};
+        x = fmincon(fun, x0, [], [], [], [], xl, xu, [], options);
+    elseif nargin == 8
+        fun = varargin{1};
+        x0 = varargin{2};
+        xl = varargin{3};
+        xu = varargin{4};
+        aub = varargin{5};
+        bub = varargin{6};
+        aeq = varargin{7};
+        beq = varargin{8};
+        x = fmincon(fun, x0, aub, bub, aeq, beq, xl, xu, [], options);
+    elseif nargin == 10
+        fun = varargin{1};
+        x0 = varargin{2};
+        xl = varargin{3};
+        xu = varargin{4};
+        aub = varargin{5};
+        bub = varargin{6};
+        aeq = varargin{7};
+        beq = varargin{8};
+        cub = varargin{9};
+        ceq = varargin{10};
+        nonlcon = @(x) deal(cub(x), ceq(x));
+        x = fmincon(fun, x0, aub, bub, aeq, beq, xl, xu, nonlcon, options);
+    end
+end
