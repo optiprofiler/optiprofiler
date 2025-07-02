7099b3b5ce37220255e886171b70e2620d1b33ab
Huang_Mac <huangcunxin@outlook.com>
2025-07-02 11:29:33 +0800

    Modify test

--- matlab/optiprofiler/tests/action_tests/stress_test.m
+++ matlab/optiprofiler/tests/action_tests/stress_test.m
@@ -25,7 +25,11 @@ function stress_test(benchmark_id)
     options.problem_names = selected_pb_list;
     options.mindim = 2000;
     options.maxdim = 10000;
-    options.max_eval_factor = 1;
+    if isunix && ~ismac
+        options.max_eval_factor = 1;
+    else
+        options.max_eval_factor = 0.2;
+    end
     options.benchmark_id = benchmark_id;
     if isunix && ~ismac
         options.plibs = {'s2mpj', 'matcutest'};
