b18dc5d7ddec1fff872a6f5ac1e47c84ddd0846a
Huang_Mac <huangcunxin@outlook.com>
2026-02-21 05:34:24 +0800

    Test workflow build

--- .github/actions/build/test_package.sh
+++ .github/actions/build/test_package.sh
@@ -8,7 +8,8 @@ for f in dist/*.tar.gz dist/*.whl; do
     source venv/bin/activate
     python -m pip install --progress-bar=off "$f[tests]"
     python -c "import optiprofiler; optiprofiler.show_versions()"
-    python -m pytest --pyargs optiprofiler
+    # Run only unit tests, not action_tests
+    python -m pytest --pyargs optiprofiler.tests
     deactivate
     rm -r venv
 done
