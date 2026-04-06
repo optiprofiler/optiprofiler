d31c752761fa1f78fd5856add40599caee7f7bf0
Huang_Mac <huangcunxin@outlook.com>
2026-04-07 02:37:17 +0800

    Release version 1.0.0

--- python/optiprofiler/__init__.py
+++ python/optiprofiler/__init__.py
@@ -12,7 +12,7 @@
 #
 # Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
 # 'X.Y.dev0' is the canonical version of 'X.Y.dev'.
-__version__ = '1.0.dev2'
+__version__ = '1.0.0'
 
 # Public API of the optiprofiler package
 from .opclasses import Feature, Problem, FeaturedProblem
