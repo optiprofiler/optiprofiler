de0c01fb964ec41cfdf8888e6a1b364e94e36b38
Huang_Mac <huangcunxin@outlook.com>
2026-04-09 16:49:13 +0800

    Improve python unit test

--- .github/actions/test-python/run-tests.sh
+++ .github/actions/test-python/run-tests.sh
@@ -7,8 +7,11 @@ set -x
 if [[ "$RUNNER_OS" == "Linux" ]]; then
   export PYCUTEST_CACHE=$GITHUB_WORKSPACE
   python -m pip install --progress-bar=off ".[extra,tests]"
-  python -m pytest --cov=. --cov-report=xml --run-extra
+  # Measure only the optiprofiler package. Using --cov=. from the repo root
+  # includes thousands of unrelated .py files (submodules, embedded problems),
+  # which makes coverage XML generation extremely slow on Windows CI.
+  python -m pytest --cov=optiprofiler --cov-report=xml --run-extra
 else
   python -m pip install --progress-bar=off ".[tests]"
-  python -m pytest --cov=. --cov-report=xml
+  python -m pytest --cov=optiprofiler --cov-report=xml
 fi
